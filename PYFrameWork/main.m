7363e25b87645a682e9182baea5b9115c9ff6ee0
飘逸A186 <wlpiaoyi@localhost>
2016-06-14 14:45:40 +0800

    Initial Commit

---
+++ PYFrameWork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PYFrameWork
+//
+//  Created by wlpiaoyi on 16/6/14.
+//  Copyright © 2016年 wlpiaoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
